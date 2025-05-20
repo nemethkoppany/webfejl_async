a0346f7d2110c614df4eeee38e60cce8d541baba
nemethkoppany <koppany344@gmail.com>
2025-05-20 20:21:19 +0200

    Órai munka kész

--- 1_promise_async/main.js
+++ 1_promise_async/main.js
@@ -3,4 +3,12 @@ const view = new DataViewController();
 
 service.init().then((value)=>{
     view.renderData(value);
-})
+});
+
+service.realinit(1).then((value)=>{
+    view.renderData(value);
+});
+
+service.realinit(3).catch((error)=>{
+    view.renderError(erro);
+});
--- 1_promise_async/service.js
+++ 1_promise_async/service.js
@@ -10,4 +10,27 @@ class Service{
             setTimeout(()=>{resolve(this.#data)},3000);
         })    
     }
+
+    initInvalid(){
+        return new Promise((reject) => {
+            setTimeout(()=> {
+                reject("Hiba");
+            })
+        })
+    }
+
+    realinit(num){
+        return new Promise((resolve, reject)=>{
+            if(num < 2){
+                setTimeout(() =>{
+                    resolve(this.#data);
+                },1000)
+            }
+            else{
+                setTimeout(()=>{
+                    reject("Hiba");
+                },1000)
+            }
+        })
+    }
 }
